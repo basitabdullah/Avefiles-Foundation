170d30b8b9f6286bf2c8367a2c34bb8a549a0c6e
numanbhat771 <sam2398.sw@gmail.com>
2024-11-20 14:52:37 +0530

    dashboard

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Dashboard.scss";
 import image from "../../assets/photo.jpg";
 import health from "../../assets/health.jpg";
+import poor from "../../assets/poor.jpg";
 import MetaData from "../../components/MetaData";
 const Dashboard = () => {
   return (
@@ -24,8 +25,40 @@ const Dashboard = () => {
           <img src={image} />
         </div>
       </div>
+      <div className="full-width-image">
+        <img src={poor} alt="Background" className="background-image" />
+        <div className="overlay-text">
+          <h1>Let's Come Togethrer</h1>
+          <p>Every Voice Needs To Be Heard</p>
+        </div>
+      </div>
+
       <div className="section-two">
-       
+        <div className="cards-container">
+          <div className="card">
+            <img src={poor} alt="Card 1" className="card-image" />
+            <div className="card-content">
+              <h3>Card Title 1</h3>
+              <p>Brief description for card 1 goes here.</p>
+              <button className="learn-more-btn">Learn More</button>
+            </div>
+          </div>
+          <div className="card">
+            <img src={poor} alt="Card 1" className="card-image" />
+            <div className="card-content">
+              <h3>Card Title 1</h3>
+              <p>Brief description for card 1 goes here.</p>
+              <button className="learn-more-btn">Learn More</button>
+            </div>
+          </div>
+          <div className="card">
+            <img src={poor} alt="Card 1" className="card-image" />
+            <div className="card-content">
+              <h3>Card Title 1</h3>
+              <p>Brief description for card 1 goes here.</p>
+              <button className="learn-more-btn">Learn More</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
