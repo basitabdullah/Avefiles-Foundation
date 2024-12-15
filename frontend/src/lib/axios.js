b87ed7bed43beba039613509cb4c83e2c7a276c2
Basit <basitabdullah1122@gmail.com>
2024-12-15 22:57:30 +0530

    Changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://157.173.219.86:5000/api",
   withCredentials: true, // send cookies to the server
 });
 
